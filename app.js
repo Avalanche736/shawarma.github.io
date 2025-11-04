162ef7a909e21a3a3624878ba7174db8650f13cb
Danil <dposyagin@gmail.com>
2025-11-04 23:39:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ tg.expand();
 
 
 const Http = new XMLHttpRequest();
-const url = 'https://07e47b317c80.ngrok-free.app/';
+const url = 'https://f1a13b65c751.ngrok-free.app/';
 Http.open("GET", url);
 Http.setRequestHeader('ngrok-skip-browser-warning', '0');
 Http.send();
